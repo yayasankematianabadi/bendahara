f8178b1256bde2a1b014d98fde2ac053373642c7
yayasankematianabadi <yayasankematianabadi@gmail.com>
2026-02-20 22:23:30 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
 import streamlit as st
 from st_supabase_connection import SupabaseConnection
 from login import show_login
-from upload_data import show_upload_dashboard
-from process_data import show_run_procedure
-from report_rekonsiliasi_transaksi_deposit_dan_settlement import show_report_deposit_settlement
-from report_rekonsiliasi_transaksi_disbursement_dan_saldo_durian import show_report_disbursement_durian
-from report_detail_reversal import show_report_detail_reversal
-from report_balance_flow import show_report_balance_flow
-from delete_data import show_delete_data
 
 # 1. SET WIDE MODE DEFAULT
 st.set_page_config(
@@ -66,51 +59,19 @@ else:
         st.write("Harap upload dan proses data terlebih dahulu sebelum menarik report!")
         st.divider()
 
-        col1, col2 = st.columns(2)
+        col1 = st.columns(1)
         with col1:
-            if st.button("📤\n\n\n\nUpload Data", key="btn_upload", use_container_width=True):
+            if st.button("📤\n\n\n\nMasukkan Data", key="btn_upload", use_container_width=True):
                 st.session_state["current_page"] = "upload"
-                st.rerun()
-        with col2: 
-            if st.button("⚙️\n\n\n\nProcess Data", key="card_proc", use_container_width=True):
-                st.session_state["current_page"] = "procedure"
-                st.rerun()
-        with col1:
-            if st.button("🗑️\n\n\n\nDelete Data", key="btn_delete", use_container_width=True):
-                st.session_state["current_page"] = "delete"
                 st.rerun()
 
         st.title("Report")
         st.divider()
-        col3, col4 = st.columns(2)
+        col3 = st.columns(1)
         with col3:
             if st.button("📊\n\n\n\nReport Rekonsiliasi Transaksi Deposit dan Settlement", key="r1", use_container_width=True):
                 st.session_state["current_page"] = "report_rekonsiliasi_transaksi_deposit_dan_settlement"
                 st.rerun()
-        with col4:
-            if st.button("📊\n\n\n\nRekonsiliasi Transaksi Disbursement dan Saldo Durian", key="r2", use_container_width=True):
-                st.session_state["current_page"] = "report_rekonsiliasi_transaksi_disbursement_dan_saldo_durian"
-                st.rerun()
-        with col3:
-            if st.button("📊\n\n\n\nReport Detail Reversal", key="r3", use_container_width=True):
-                st.session_state["current_page"] = "report_detail_reversal"
-                st.rerun()
-        with col4:
-            if st.button("📊\n\n\n\nReport Balance Flow", key="r4", use_container_width=True):
-                st.session_state["current_page"] = "report_balance_flow"
-                st.rerun()
 
     elif st.session_state["current_page"] == "upload":
         show_upload_dashboard(conn)
-    elif st.session_state["current_page"] == "procedure":
-        show_run_procedure(conn)
-    elif st.session_state["current_page"] == "report_rekonsiliasi_transaksi_deposit_dan_settlement":
-        show_report_deposit_settlement(conn)
-    elif st.session_state["current_page"] == "report_rekonsiliasi_transaksi_disbursement_dan_saldo_durian":
-        show_report_disbursement_durian(conn)
-    elif st.session_state["current_page"] == "report_detail_reversal":
-        show_report_detail_reversal(conn)
-    elif st.session_state["current_page"] == "report_balance_flow":
-        show_report_balance_flow(conn)
-    elif st.session_state["current_page"] == "delete":
-        show_delete_data(conn)
